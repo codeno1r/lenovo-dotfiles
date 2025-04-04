19e8adcd3dd0154875efccef8725c6acffeac24a
codeno1r <gvarrworks@gmail.com>
2025-04-04 16:15:57 +0800

    added customization for 2nd monitor

--- scripts/.local/bin/vpn.sh
+++ scripts/.local/bin/vpn.sh
@@ -3,7 +3,7 @@
 nordstatus=$(nordlayer status --silent | grep VPN | sed s/"VPN: "//)
 
 if [ "$nordstatus" = "Connected" ]; then
-  printf '{"text": "󰳌", "alt": "VPN Connected", "tooltip": "VPN is active", "class": "connected"}'
+  printf '{"text": "󰳌 ON", "alt": "VPN Connected", "tooltip": "VPN is active", "class": "connected"}'
 else
-  printf '{"text": "󰯄", "alt": "VPN Disconnected", "tooltip": "VPN is off", "class": "disconnected"}'
+  printf '{"text": "󰯄 OFF", "alt": "VPN Disconnected", "tooltip": "VPN is off", "class": "disconnected"}'
 fi
